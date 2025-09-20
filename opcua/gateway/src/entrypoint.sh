4307854b4c5efd429b3a9158d67e90e331cdbd67
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-09-19 20:49:59 -0400

    Wait for coordinator

--- opcua/gateway/src/entrypoint.sh
+++ opcua/gateway/src/entrypoint.sh
@@ -2,13 +2,25 @@
 # entrypoint.sh
 
 # Default coordinator URL
-COORDINATOR_URL=${COORDINATOR_URL:-http://coordinator:8000}
+COORDINATOR_URL=${COORDINATOR_URL:-http://opcua-coordinator:8000}
 
 # Gateway container ID or name
+if [ -z "$GATEWAY_HOST" ]; then
+    echo "ERROR: GATEWAY_HOST is not set"
+    exit 1
+fi
 GATEWAY_HOST=${GATEWAY_HOST}
 
+# Wait for HTTP to respond
+until curl -sSf $COORDINATOR_URL/assignments; do
+    echo "Waiting for coordinator service ..."
+    sleep 5
+done
+echo
+
 # Register with coordinator
-echo "Registering gateway $GATEWAY_HOST with coordinator..."
+echo "Registering gateway $GATEWAY_HOST with coordinator ..."
+
 curl -X POST "$COORDINATOR_URL/register_gateway" \
      -H "Content-Type: application/json" \
      -d "{\"gateway_host\":\"http://$GATEWAY_HOST:8001\"}"
