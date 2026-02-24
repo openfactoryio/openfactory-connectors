7e4ae312a059a5b6f94262dafeab232def75213e
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2026-02-24 15:49:26 +0000

    Add missing bootstrap_servers

--- opcua/debug/send_cmd_to_asset.py
+++ opcua/debug/send_cmd_to_asset.py
@@ -1,3 +1,4 @@
+import os
 from uuid import uuid4
 from datetime import datetime, timezone
 import openfactory.config as config
@@ -8,7 +9,7 @@ from openfactory.schemas.command_header import CommandEnvelope, CommandHeader
 ksql = KSQLDBClient(config.KSQLDB_URL)
 
 # Example 1: temp sensor
-temp_sensor = Asset('OPCUA-SENSOR-001', ksqlClient=ksql)
+temp_sensor = Asset('OPCUA-SENSOR-001', ksqlClient=ksql, bootstrap_servers=os.getenv("KAFKA_BROKER"))
 print("Requesting Calibrate method ...")
 envelope = CommandEnvelope(
     header=CommandHeader(
@@ -22,7 +23,7 @@ envelope = CommandEnvelope(
 temp_sensor.Calibrate_CMD = envelope.model_dump_json()
 
 # Example 2: barcode reader - SetManualMode
-barcode_reader = Asset('VIRTUAL-BARCODE-READER', ksqlClient=ksql)
+barcode_reader = Asset('VIRTUAL-BARCODE-READER', ksqlClient=ksql, bootstrap_servers=os.getenv("KAFKA_BROKER"))
 print("Requesting SetManualMode method ...")
 envelope = CommandEnvelope(
     header=CommandHeader(
