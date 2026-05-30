93de36c4f5bdf7925f7fd4df9c59061fa1393b70
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2026-05-30 21:37:45 +0000

    Initial commit

---
+++ tests/connectors/shdr/gateway/test_gateway_config.py
@@ -0,0 +1,20 @@
+import unittest
+from connectors.common.gateway import BaseGateway
+from connectors.shdr.gateway.src.main import SHDRGateway
+
+
+class SHDRGatewayConfigurationTests(unittest.TestCase):
+    """
+    Unittests for SHDRGateway configuration.
+    """
+
+    def test_is_a_base_gateway(self):
+        """ Test SHDRGateway inherits from BaseGateway. """
+        self.assertTrue(issubclass(SHDRGateway, BaseGateway))
+
+    def test_connector_name(self):
+        """ Test SHDRGateway connector name. """
+        self.assertEqual(SHDRGateway.CONNECTOR_NAME, "SHDR")
+
+if __name__ == "__main__":
+    unittest.main()
